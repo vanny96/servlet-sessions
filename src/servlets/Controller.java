dd8988cccd08d01dcc67922ca1f33cdb4cea6e24
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-07-10 08:01:38 +0200

    First and last commit

---
+++ src/servlets/Controller.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if(username.equals("bob") && password.equals("abc")) {
+			request.getSession().invalidate();
+			
+			HttpSession newSession = request.getSession(true);
+			newSession.setMaxInactiveInterval(600);
+			newSession.setAttribute("username", "bob");
+			
+			request.getRequestDispatcher("/session.jsp").forward(request, response);
+			
+		} else {
+			response.sendRedirect(request.getContextPath() + "/login.jsp");
+		}
+	}
+
+}
